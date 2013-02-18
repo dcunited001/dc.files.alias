f4238e66e74842c332b59fc775fc841b7a4831c6
David Conner <dconner.pro@gmail.com>
2013-02-17 20:47:58 -0500

    added functions to insert lines after a specified anchor pattern using AWK.
    NOTE: FRIENDS DON'T LET FRIENDS REPLACE TEXT IN-FILE WITH AWK,
      WHEN SED WIILL DO JUST FINE!! lol
    
    jeez, well know i know how to manipulate temp files i guess..

--- func.sh
+++ func.sh
@@ -1,2 +1,54 @@
 #!/bin/bash
 # shell functions for all platforms
+
+#===============================
+# Awk Functions
+#===============================
+# Examples
+# - http://awk.info/?EdM
+# - http://awk.info/?awk1line
+# - http://www.grymoire.com/Unix/Awk.html#uh-4
+# - http://www.ibm.com/developerworks/library/l-awk1/
+
+awk-insert-above(){
+  file=$1; rgx=$2; str=$3;
+  script="{ print (/$rgx/ ? \"$str\\n\" : \"\") \$0 } "
+  awk $script $file; }
+
+awk-insert-below(){
+  file=$1; pattern=$2; str=$3;
+  script="{ print \$0 (/$rgx/ ? \"\\n$str\" : \"\"); } "
+  awk $script $file; }
+
+awk-insert-below-and-replace(){
+  filefoo=`get-temp-file awk-insert`;
+  awk-insert-below $@[@] > $filefoo;
+  cp $file $file.bak; mv $filefoo $file; }
+
+awk-insert-above-and-replace(){
+  filefoo=`get-temp-file awk-insert`;
+  awk-insert-above $@[@] > $filefoo;
+  cp $file $file.bak; mv $filefoo $file; }
+
+#===============================
+# Sed Functions
+#===============================
+  #sed '/regex/{x;p;x;}'    #insert-above
+  #sed '/regex/G'           #insert-below
+  #sed '/regex/{x;p;x;G;}'  #insert-above-below
+
+#===============================
+# Mktemp Functions
+#===============================
+# hmmm..  probably need bash pointers
+#   and seriously? f*** that right now
+#copy-thru-temp(){
+  #hmmmmm..  what params?  name of function & input array?
+#}
+
+get-temp-file(){
+  #hmmmm, "thread" safe vars? lol .. YIKES!
+  tmpfoo=`basename $1`
+  tmpfile=`mktemp /tmp/${tmpfoo}.XXXXXX` || exit 1
+  echo $tmpfile
+}
