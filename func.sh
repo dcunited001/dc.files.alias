8492bf95a85ea02060f5c0b11302300c12a75331
David Conner <dconner.pro@gmail.com>
2013-03-20 02:36:40 -0600

    added new git functions for syncing a branch to master. added push/pop shift/unshift functions for bash.

--- func.sh
+++ func.sh
@@ -1,6 +1,100 @@
 #!/bin/bash
 # shell functions for all platforms
 
+
+
+#===============================
+# Git Macros
+#===============================
+alias gsm="git-sync-master"
+git-sync-master(){
+    git checkout master &&
+    git fetch origin &&
+    git pull origin master &&
+    git checkout $1 &&
+    git rebase master; }
+
+#!/bin/sh -x
+###git name-rev is fail
+#CURRENT=`git branch | grep '\*' | awk '{print $2}'`
+#git checkout master
+#git fetch
+#git pull origin master
+#git checkout ${CURRENT}
+#git rebase master
+
+#https://gist.github.com/cadams500/1902385
+# sync local tags with remote
+#git tag -l | xargs git tag -d
+#git fetch
+
+#git remove local merged branches
+#git branch --merged master | grep -v 'master$' | xargs git branch -d
+
+# git branches by date
+#alias git-branches-by-date='git branch | xargs -n1 ~/bin/branch-date | sort'
+
+# # Rebase all branches on top of origin/master (or origin/staging for staging)
+# # https://github.com/mikemcquaid/scripts/blob/master/git-rebase-all-branches
+# #!/bin/sh
+
+# GIT_CURRENT_BRANCH=$(git symbolic-ref HEAD|sed -e 's/refs.heads.//')
+
+# git fetch --all
+
+# git checkout staging && git rebase origin/staging
+# for i in $(git branch|egrep -v '\*|staging')
+# do
+#     git checkout $i && git rebase origin/master
+# done
+
+# git checkout $GIT_CURRENT_BRANCH
+
+#http://stackoverflow.com/questions/2928584/how-to-grep-search-committed-code-in-the-git-history
+#
+#To search for commit content
+#(i.e., actual lines of source, as opposed to commit messages and the like), what you need to do is:
+
+# git grep <regexp> $(git rev-list --all)
+# This will grep through all your commit text for regexp.
+
+# Here are some other useful ways of searching your source:
+
+# Search working tree for text matching regular expression regexp:
+
+# git grep <regexp>
+# Search working tree for lines of text matching regular expression regexp1 or regexp2:
+
+# git grep -e <regexp1> [--or] -e <regexp2>
+# Search working tree for lines of text matching regular expression regexp1 and regexp2, reporting file paths only:
+
+# git grep -e <regexp1> --and -e <regexp2>
+# Search working tree for files that have lines of text matching regular expression regexp1 and lines of text matching regular expression regexp2:
+
+# git grep -l --all-match -e <regexp1> -e <regexp2>
+# Search all revisions for text matching regular expression regexp:
+
+# git grep <regexp> $(git rev-list --all)
+# Search all revisions between rev1 and rev2 for text matching regular expression regexp:
+
+# git grep <regexp> $(git rev-list <rev1>..<rev2>)
+
+#===============================
+# Push/Pop & Shift/Unshift
+#===============================
+bpush() { arr=("${arr[@]}" "$1") }
+bshift() { arr=("$1" "${arr[@]}") }
+
+bpop() {
+  i=$(expr ${#arr[@]} - 1)
+  placeholder=${arr[$i]}
+  unset arr[$i]
+  arr=("${arr[@]}") }
+bunshift() {
+  placeholder=${arr[0]}
+  unset arr[0]
+  arr=("${arr[@]}") }
+
 #===============================
 # Alias Functions
 #===============================
