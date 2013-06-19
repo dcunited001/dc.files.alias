f3f814fa725b879899fc3cb55b98f17a0a9efab5
David Conner <dconner.pro@gmail.com>
2013-06-19 05:35:27 -0600

    smartgit alias

--- alias.ubu.sh
+++ alias.ubu.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
 source $HOME/.files/alias/alias.sh
 
+#==========================
+# MISC APPLICATIONS
+#==========================
+smgit(){ smartgit.sh --open $1 &; }
+alias str=smgit
+
 #__APALI__#
