2efadacf63113bfb578f6484b907f01f6038dc1d
David Conner <dconner.pro@gmail.com>
2013-06-19 04:39:59 -0600

    more git aliases

--- alias.sh
+++ alias.sh
@@ -348,6 +348,9 @@ alias apaliu="insert-alias $DFA/alias.ubu.sh $APALI_REGEX "
 # TODO: just change it to use sed. FML lol
 
 #__APALI__#
+alias grea='git rebase --abort'
+alias gcl='git clone'
+alias gres='git reset'
 alias shc='ssh-config'
 alias glrom='git pull --rebase origin master'
 alias pc='pbcopy < '
