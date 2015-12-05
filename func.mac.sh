1bebe44193bb9a0b6ca979e2e9cdd1c4cc3a9cb7
David Conner <dconner.pro@gmail.com>
2015-12-05 15:29:41 -0500

    added setjdk function, but not really sure if i'm using it

--- func.mac.sh
+++ func.mac.sh
@@ -402,3 +402,20 @@ EOF
 backup-plist () {
   cp $1 .
   plutil -convert xml1 -o $1.xml $1 }
+
+function setjdk() {
+if [ $# -ne 0 ]; then
+removeFromPath '/System/Library/Frameworks/JavaVM.framework/Home/bin'
+if [ -n "${JAVA_HOME+x}" ]; then
+removeFromPath $JAVA_HOME
+fi
+export JAVA_HOME=/usr/libexec/java_home -v $@
+export PATH=$JAVA_HOME/bin:$PATH
+fi
+}
+function removeFromPath() {
+export PATH=$(echo $PATH | sed -E -e "s;:$1;;" -e "s;$1:?;;")
+}
+
+
+
